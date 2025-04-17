531e27f2aadb4f5fa69ca7557f04755ca2d2f2a3
Mehdi Bellam <porcepic91@gmail.com>
2025-04-17 10:30:33 +0200

    toute les rareté on été récupérer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 CarteRecherché = input("quel carte voulez-vous ?")
 driver = webdriver.Chrome()
 # CarteRecherché = "Numéro 17 : Dragon Léviathan"
@@ -13,7 +14,8 @@ print(10)
 driver.find_element(By.XPATH , "/html/body/header/nav[2]/form/div[1]/div/input[1]").send_keys(CarteRecherché)
 print(15)
 
-# /html/body/header/nav[2]/form/div/div/button
+
+
 # cliquer sur le boutton afin de lancer la recherche
 driver.find_element(By.XPATH , "/html/body/header/nav[2]/form/div/div/button").click()
 print(18)
@@ -23,9 +25,57 @@ print(18)
 print(20)
 #//*[@id="idRarity67ffc29375a99"]
 # driver.find_element(By.XPATH , '[@id="idRarity67ffc29375a99"]').click()
+time.sleep(4)
 
-# element = driver.find_element(By.XPATH, "/html/body/main/section/div[3]/div[2]/div/a[2]")
+# je change la disposition en galerie
+element = driver.find_element(By.XPATH, "/html/body/main/section/div[3]/div[2]/div/a[2]").click()
 # href = element.get_attribute("href")
 # print(href)
+#  
+# choisir la rareté
+driver.find_element(By.XPATH, "/html/body/main/section/div[1]/form/div/div[7]/div[1]/div/button").click()
+
+# avoir toute les rareté
+# /html/body/main/section/div[1]/form/div/div[7]/div[2]/div/div[1]/select
+time.sleep(5)
+select_element = driver.find_element(By.XPATH, '/html/body/main/section/div[1]/form/div/div[7]/div[2]/div/div[1]/select')
+select = Select(select_element)
+option_list = select.options
+ListeRareté = [
+    'Tout',
+    'Common',
+    'Rare',
+    'Parallel Rare',
+    'Starfoil Rare',
+    'Mosaic Rare',
+    'Shatterfoil',
+    'Super Rare',
+    'Super Parallel Rare',
+    'Ultra Rare',
+    'Ultra Parallel Rare',
+    'Gold Rare',
+    'Premium Gold Rare',
+    'Gold Secret Rare',
+    'Secret Rare',
+    'Secret Parallel Rare',
+    'Extra Secret Rare',
+    'Platinum Rare',
+    'Platinum Secret Rare',
+    'Collectors Rare',
+    'Quarter Century Secret Rare',
+    'Ultimate Rare',
+    'Ghost Rare',
+    'Starlight Rare',
+    'Holographic Rare',
+    'Special',
+    'Token',
+    'Oversized',
+    'Unknown'
+]
+
+for option in option_list:
+    ListeRareté.append(option.text)
+    print(option.text)
+print(ListeRareté)
 print(21)
 time.sleep(60)
